b159cc81e1290836f45aafa1de9e62dad12671e6
h010d <lessonsep@gmail.com>
2018-04-07 11:34:26 +0300

    adaptive

---
+++ 9/9/scripts/html5-ie-fix.js
@@ -0,0 +1,8 @@
+document.createElement('header');
+document.createElement('nav');
+document.createElement('section');
+document.createElement('article');
+document.createElement('menu');
+document.createElement('footer');
+document.createElement('time');
+document.createElement('main');
